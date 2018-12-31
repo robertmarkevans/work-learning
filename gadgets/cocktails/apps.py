af15750b7786255dd39af7d380199119a591ac8c
rmevans <robertmarkevans@gmail.com>
2018-12-31 14:24:54 +0000

    Added DRM Initial stuff

--- gadgets/cocktails/apps.py
+++ gadgets/cocktails/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class PhotosConfig(AppConfig):
-    name = 'photos'
+class CocktailsConfig(AppConfig):
+    name = 'cocktails'
--- gadgets/cocktails/views.py
+++ gadgets/cocktails/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from gadgets.cocktails.serializers import UserSerializer, GroupSerializer
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the cocktails index.")
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
